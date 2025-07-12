7c07913f867677350e72111cc49098e53bb6414b
Sajib99design <sajib99design@gmail.com>
2025-07-12 15:33:20 +0600

    hero add

---
+++ src/componets/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button } from '@heroui/react'
+function Hero() {
+    return (
+        <section className="bg-[url(./hero.png)] bg-center bg-no-repeat bg-cover md:py- 28 py-14 ">
+            <div className="space-y-6  max-w-[1200px] mx-auto">
+                <h4 className='text-2xl text-cyan'>AI SDRs (aiDRs)</h4>
+                <h2 className="font-bold  text-white md:text-[38px]  lg:text-[64px] leading-[5rem] ">
+                    More leads, <br /> less people.
+                </h2>
+
+                <p className="text-secondary w-4/5 text-[20px] lg:text-sm xl:text-base">
+                    Train an aiDR on your ICP and messaging matrix. <br />Activate it on a patch. It will send personalized  <br /> sequences to every target contact.
+                </p>
+
+                <button  className='bg-cyan font-medium text-foreground py-4 px-5 rounded-full  hover:bg-white transition hover:text-cyan'>
+                    Sign Up for the Beta <img className='inline-block' src="./arrow.png" alt="" />
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
