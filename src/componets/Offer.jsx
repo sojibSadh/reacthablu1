1ab0353b8e71a33bd1bbb2ebf43eb157f434d85d
Sajib99design <sajib99design@gmail.com>
2025-07-12 17:03:00 +0600

    offer add

---
+++ src/componets/Offer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Offer1 from '../assets/Offer1.png';
+import Offer2 from '../assets/Offer2.png';
+import Offer3 from '../assets/Offer3.png';
+import { section } from 'framer-motion/client';
+
+
+
+function Offer() {
+    const OfferList = [
+        {
+            icon: Offer1,
+            title: 'You’re in Control',
+            description: 'aiDRs operate based on your rules. You guide the machine, the aiDRs execute the work.'
+        },
+        {
+            icon: Offer2,
+            title: 'Infinitely Scalable',
+            description: 'Train an aiDR on a patch. When you are happy with the results, scale it effortlessly.'
+        },
+        {
+            icon: Offer3,
+            title: 'Incredibly Flexible',
+            description: 'Train an aiDR on an outbound patch, to nurture inbound leads, or to reach out to your long tail of SMB customers at renewal time.'
+        }
+    ]
+    return (
+        <section className='bg-[#002228]'>
+            <div className='max-w-[1200px] mx-auto py-[120px]'>
+                <div className='gap-6 grid grid-cols-1 md:grid-cols-3'>
+                    {
+                        OfferList.map((item, i) => (
+                            <div key={i} className='pr-5'>
+                                <div>
+                                    <img src={item.icon} alt="offer" />
+                                </div>
+                                <div>
+                                    <h4 className='text-[28px] text-white py-3 font-semibold'>{item.title}</h4>
+                                    <p className='text-[22px] text-gray/80 font-light'>{item.description}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Offer
