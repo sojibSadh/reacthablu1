4d93daf3235e185402d647aa07f6f77b98f9298f
Sajib99design <sajib99design@gmail.com>
2025-07-12 17:01:36 +0600

    brand

---
+++ src/componets/Brand.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Brand() {
+    return (
+        <section className='py-8'>
+            <h4 className='text-sm text-cyan text-center uppercase mb-4'>our trusted partners</h4>
+            <div className='text-center'>
+                <img className='mx-auto block' src="./brand.png" alt="" />
+            </div>
+        </section>
+    )
+}
+
+export default Brand
